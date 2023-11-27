be54b6a2bd17ac9c9b1c3c4a9d7b6f0861ed282b
mennatabuelnaga <mennat.abuelnaga@gmail.com>
2023-11-27 11:16:13 +0200

    feat: add generate_random_array_json()

--- scripts/common.sh
+++ scripts/common.sh
@@ -11,7 +11,7 @@ done
 # wasm_execute <EXECUTE_MSG> <AMOUNT>
 wasm_execute() {
 
-    OUTPUT="$(seda-chaind tx wasm execute $POC_CONTRACT_ADDRESS $1 --from $DEV_ACCOUNT  --keyring-backend test --node $RPC_URL --gas-prices 0.1aseda --gas auto --gas-adjustment 1.3 -y --output json --chain-id $CHAIN_ID --amount "$2"seda)"
+    OUTPUT="$(wasmd tx wasm execute $POC_CONTRACT_ADDRESS $1 --from $DEV_ACCOUNT --node $RPC_URL --gas-prices 0.025stake --gas auto --gas-adjustment 1.3 -y --output json --chain-id $CHAIN_ID --amount "$2"stake)"
     echo $OUTPUT
 
     TXHASH=$(echo "$OUTPUT" | jq -r '.txhash')
@@ -23,7 +23,7 @@ wasm_execute() {
 # sudo_call <SUDO_MSG>
 sudo_call() {
 
-    OUTPUT="$(seda-chaind tx gov submit-proposal sudo-contract $POC_CONTRACT_ADDRESS $1 --from $DEV_ACCOUNT  --keyring-backend test --node $RPC_URL --gas-prices 0.1aseda --gas auto --gas-adjustment 1.3 -y --output json --chain-id $CHAIN_ID)"
+    OUTPUT="$(wasmd tx gov submit-proposal sudo-contract $POC_CONTRACT_ADDRESS $1 --from $DEV_ACCOUNT --node $RPC_URL --gas-prices 0.025stake --gas auto --gas-adjustment 1.3 -y --output json --chain-id $CHAIN_ID)"
     echo $OUTPUT
 
     TXHASH=$(echo "$OUTPUT" | jq -r '.txhash')
@@ -37,8 +37,43 @@ sudo_call() {
 # wasm_query <QUERY_MSG>
 wasm_query() {
 
-    OUTPUT="$(seda-chaind query wasm contract-state smart $POC_CONTRACT_ADDRESS $1 --node $RPC_URL --output json --chain-id $CHAIN_ID)"
+    OUTPUT="$(wasmd query wasm contract-state smart $POC_CONTRACT_ADDRESS $1 --node $RPC_URL --output json --chain-id $CHAIN_ID)"
     echo $OUTPUT
 
     sleep 10
+}
+
+
+
+
+
+# Function to generate a random array of strings as JSON
+generate_random_array_json() {
+    local array_size=$1
+    local string_length=$2
+
+    # Check if array size and string length are provided
+    if [ -z "$array_size" ] || [ -z "$string_length" ]; then
+        echo "Usage: generate_random_array_json <array_size> <string_length>"
+        return 1
+    fi
+
+    # Generate random strings and populate the array
+    local random_array=()
+    for ((i = 0; i < array_size; i++)); do
+        random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c $string_length)
+        random_array+=("$random_string")
+    done
+
+    # Convert the array to JSON format
+    json_output='{"results":['
+    for ((i = 0; i < ${#random_array[@]}; i++)); do
+        json_output+="\"${random_array[i]}\""
+        if [ $i -lt $(( ${#random_array[@]} - 1 )) ]; then
+            json_output+=','
+        fi
+    done
+    json_output+=']}'
+
+    echo "$json_output"
 }
--- scripts/execute.sh
+++ scripts/execute.sh
@@ -2,6 +2,15 @@
 
 # usage: bash scripts/execute.sh -c "sedachain" -d "seda12nmulx864e9ggymhf3tmavrmr6lse9l3qr4f4q" -r "http://127.0.0.1:26657" -p seda12tc6cpv3e49z2me3xcm3064zkvrd7suqh3xaages9kq6adnnhjxq7ehgyk
 
+
+# bash scripts/execute.sh -c "my-chain" -d "wasm1kemm5uhf2rqzv8l7a3adj807c9kw66sauu2v77" -r "http://127.0.0.1:26657" -p "wasm14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s0phg4d"
 source scripts/common.sh
 
-wasm_execute '{"post_results":{"results":["hello"]}}' 0
+
+# Example usage: generate a random array of 5 strings, each with a length of 8
+generated_array=$(generate_random_array_json 10000 8)
+# echo "Random Array: ${generated_array}"
+
+
+# echo '{"post_results":'${generated_array}'}'
+wasm_execute '{"post_results":'${generated_array}'}' 0
